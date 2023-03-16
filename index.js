e8c7d94ebc37c287005a67075d83f14420159417
williamkeane112 <williamkeanekeane@gmail.com>
2023-03-16 15:00:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const navMenu = document.querySelector('.nav-menu');
+// const hamMenu = document.querySelector('#ham-menu').onclick = () => {
+//     navMenu.classList.toggle('active')
+// }
+const navMenu = document.querySelector('.nav-menu');
+const hamMenu = document.querySelector
+('#ham-menu').onclick = () => {
+  navMenu.classList.toggle('active')
+}
---
+++ index1.js
@@ -0,0 +1,5 @@
+const navMenu = document.querySelector('.nav-menu');
+const hamMenu = document.querySelecto
+('#ham-menu').onclick = () => {
+  navMenu.classList.toggle('active')
+}
